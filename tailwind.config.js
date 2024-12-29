aa7ff4f426b77432bf7c803e78f6060adbbed8f3
Anestezia-zip <plekatybtc@gmail.com>
2024-12-29 15:22:52 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        zentry: ["zentry", "sans-serif"],
+        general: ["general", "sans-serif"],
+        "circular-web": ["circular-web", "sans-serif"],
+        "robert-medium": ["robert-medium", "sans-serif"],
+        "robert-regular": ["robert-regular", "sans-serif"],
+      },
+      colors: {
+        blue: {
+          50: "#dfeff0",
+          75: "#e3f2df",
+          100: "#f4f0fa",
+          200: "#010101",
+          300: "#4fd4dd",
+        },
+        violet: {
+          300: "#5724ff",
+        },
+        yellow: {
+          100: "#98673f",
+          300: "#ffb366",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
